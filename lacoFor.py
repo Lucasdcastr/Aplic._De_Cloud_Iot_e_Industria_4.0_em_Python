2f6df04ba3c424950053888d402636da35104e90
Carlos Henrique <carloshav44@gmail.com>
2025-04-10 20:41:30 -0300

    segundo

---
+++ lacoFor.py
@@ -0,0 +1,10 @@
+# criando laco for
+# for variavel in range(10):
+#     print("Ola, mundo")
+lista = ["Carlos", "Luka", "Paulo", "Erik", "Adler", "Revelino", "Jose", "Davi", "Vitor"]
+for nome in lista:
+    print(nome)
+
+    print(lista[0])
+
+    print(lista[8])     
